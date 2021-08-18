52cf8c52d40f37b45bb58afe0f720fc3dbad4e33
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2021-08-18 10:50:44 +0800

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SecretLab Coding Exercise</title>
+        <title>Secretlab Coding Exercise</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <h3>Welcome to SecretLab Coding Exercise</h3>
+            <h3>Welcome to Secretlab Coding Exercise</h3>
             {{-- @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
